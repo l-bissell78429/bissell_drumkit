724a856afcf989908e89f417c17f8d48dc55ea0e
l-bissell78429 <43183061+l-bissell78429@users.noreply.github.com>
2019-03-04 12:25:44 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-a
+(() => {
+	//hook up the JS file
+	console.log('fired! ready to jam out!');
+
+	function playDrumSound(e) {
+		// e in round brackets is the event object ->
+		// gets generated with every event and apsses some data through
+		let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+		// the return keyword stops javaScript from running any code below it
+		if(!audio) {
+			return;
+		}
+
+		// play the audio track
+		audio.currentTime = 0;
+		audio.play();
+	}
+
+	window.addEventListener('keydown', playDrumSound);
+})();
